cf87e76323d89cdd4229075c2b6f3484e0945281
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-07-19 10:47:02 -0300

    filme.php feito

--- filme.php
+++ filme.php
@@ -1,5 +1,16 @@
 <?php
+	include('./includes/filmes.php');
 	include('./includes/generos.php');
+
+	if(!array_key_exists('id',$_GET) || $_GET['id'] >= count($filmes) ){
+		echo "FILME INEXISTENTE";
+		
+		die();
+	}
+
+	$id = 1 * $_GET['id'];
+	$f = $filmes[$id];
+
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +20,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Turma da Mônica - Laços - Clube do Filme</title>
+	<title><?= $f['titulo']; ?></title>
 
 	<!-- Meu estilo -->
 	<link rel="stylesheet" href="./assets/css/geral.css">
@@ -25,33 +36,34 @@
 				}
 			?>
 		</ul>
-		<form>
-			<input type="text" name="trecho"><button type="submit">Buscar</button>
+		<form method="GET" action="busca.php">
+			<input type="text" name="trecho">
+			<button type="submit">Buscar</button>
 		</form>
 	</nav>
 	<main>
 
-		<h2>Turma da Mônica - Laços</h2>
-		<img src="./assets/img/cartazes/cartaz-3.png" alt="Turma da Mônica - Laços" class="cartaz">
+		<h2><? = $f['titulo']; ?></h2>
+		<img src="./assets/img/cartazes/cartaz-<?= $id ?>.png" alt="<?= $f['titulo'] ?>" class="cartaz">
 		<section>
 
 			<div class="sinopse">
 				<h5>Sinopse</h5>
-				<div>Floquinho, o cachorro do Cebolinha (Kevin Vechiatto), desapareceu. O menino desenvolve então um plano infalível para resgatar o cãozinho, mas para isso vai precisar da ajuda de seus fiéis amigos Mônica (Giulia Benite), Magali (Laura Rauseo) e Cascão (Gabriel Moreira). Juntos, eles irão enfrentar grandes desafios e viver grandes aventuras para levar o cão de volta para casa. Classificação Indicativa livre, sem restrições.</div>
+				<div><?= $f['sinopse']; ?></div>
 			</div>
 
 			<div class="censura">
 				<h5>Censura</h5>
-				<div>Livre</div>
+				<div><?= $f['censura'] == 0 ? 'Livre' : $f['censura']; ?></div>
 			</div>
 			
 			<div class="critica">
 				<h5>Crítica</h5>
-				<div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, veniam quia. Quam aperiam eius facere provident officiis suscipit optio earum laudantium aut necessitatibus corrupti qui, dolorem, illum veniam doloribus quae?</div>
+				<div><?= $f['critica']; ?></div>
 			</div>
 		</section>
 
-		<iframe src="https://www.youtube.com/embed/A4k3QGPr7oQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>		
+		<?= $f['trailer']; ?>		
 	</main>
 </body>
 
